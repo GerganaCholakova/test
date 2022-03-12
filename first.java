d5ec906203a80a74375712194bb6169a00a16c01
GerganaCholakova <100899376+GerganaCholakova@users.noreply.github.com>
2022-03-12 12:59:20 +0200

    Add files via upload

---
+++ first.java
@@ -0,0 +1,18 @@
+package forth;
+
+}
+
+public class first {
+    public static void main(String[] args) {
+
+        String myName = "Gergana";
+        String mLower = myName.toLowerCase();
+        String mUpper = myName.toUpperCase();
+        
+
+        System.out.println(myName);
+        System.out.println(mLower);
+        System.out.println(mUpper);
+
+    }
+}
